bd8def37e185f7db31cf35972ea2b50593d069ee
krithik-p <63632669+krithik-p@users.noreply.github.com>
2022-08-24 14:57:16 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,6 +5,8 @@ public class hello {
 		System.out.println("Hello World");
 		System.out.println("How you doing?");
 		System.out.println("Have a great day");
+		System.out.println("How you doing?");
+		System.out.println("Have a great day");
 	}
 
 }
